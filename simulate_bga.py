bc7cb9ed4d6efe2cba3309707ec5e804cb7d108a
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2022-02-19 07:10:52 -0500

    simulate_bga.py: Gives up naive reconstruction

--- simulate_bga.py
+++ simulate_bga.py
@@ -6,43 +6,41 @@ import numpy as np
 from skimage.transform import radon
 import ast
 import math
-from skimage.transform import radon, rescale
+from skimage.transform import radon, rescale, iradon_sart, iradon
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(message)s')
 
 def main(
         imageSizeHW,
-        outputDirectory,
-        numberOfAngles
+        outputDirectory
 ):
     logging.info("simulate_bga.main()")
 
     if not os.path.exists(outputDirectory):
         os.makedirs(outputDirectory)
 
+    number_of_angles = max(imageSizeHW)
+
     # Simulate BGA density
     bga_density_img = GenerateBGADensity(imageSizeHW)
-    #cv2.imwrite(os.path.join(outputDirectory, "simulateBga_main_density.png"), bga_density_img)
 
     # Create an image of line integrals
     logging.info("Computing the Radon transform the naïve way...")
     number_of_rhos = math.ceil(math.sqrt(imageSizeHW[0]**2 + imageSizeHW[1]**2))  # The image diagonal defines the highest number of rho's
     rho_max = round(math.sqrt(imageSizeHW[0]**2 + imageSizeHW[1]**2)/2)
-    line_integrals_arr = np.zeros((number_of_rhos, numberOfAngles), dtype=np.float32)
     center = (imageSizeHW[1]/2, imageSizeHW[0]/2)
-    for thetaNdx in range(numberOfAngles):
-        theta = -math.pi/2 + thetaNdx * math.pi/numberOfAngles
-        for rhoNdx in range(number_of_rhos):
-            rho = -rho_max + rhoNdx
-            line_integral = LineIntegral(bga_density_img, rho, theta, rho_max, center)
-            line_integrals_arr[number_of_rhos - rhoNdx - 1, thetaNdx] = line_integral  # Negative rho is at bottom, positive rho is at top
-
-    cv2.imwrite(os.path.join(outputDirectory, "simulateBga_main_lineIntergrals.png"), line_integrals_arr.astype(np.uint8))
+    naive_sinogram = NaiveRadonTransform(bga_density_img, number_of_angles, number_of_rhos, rho_max, center)
+    cv2.imwrite(os.path.join(outputDirectory, "simulateBga_main_naiveSinogram.png"), naive_sinogram.astype(np.uint8))
 
     logging.info("Computing the Radon transform the efficient way, with skimage...")
-    thetas = np.linspace(0., 180., max(imageSizeHW), endpoint=False)
-    sinogram = radon(bga_density_img, theta=thetas, circle=False)
-    cv2.imwrite(os.path.join(outputDirectory, "simulateBga_main_sinogram.png"), sinogram)
+    thetas = np.linspace(0., 180., number_of_angles, endpoint=False)
+    skimage_sinogram = radon(bga_density_img, theta=thetas, circle=False)
+    cv2.imwrite(os.path.join(outputDirectory, "simulateBga_main_skimageSinogram.png"), skimage_sinogram)
+
+    reconstruction_iradon = iradon(naive_sinogram, theta=thetas, filter_name='ramp')
+    cv2.imwrite(os.path.join(outputDirectory, "simulateBga_main_reconstructionIradon.png"),
+                (np.clip(255 * reconstruction_iradon, 0, 255)).astype(np.uint8))
+
 
 def GenerateBGADensity(image_sizeHW, grid_size=(16, 16), ball_diameter=8, defect_location=(98, 142)):
     density_img = np.zeros(image_sizeHW, dtype=np.uint8)
@@ -68,6 +66,15 @@ def LineIntegral(density_arr, rho, theta, rho_max, center):
         running_rho += 1.0
     return line_integral
 
+def NaiveRadonTransform(density_arr, number_of_angles, number_of_rhos, rho_max, center):
+    line_integrals_arr = np.zeros((number_of_rhos, number_of_angles), dtype=np.float32)
+    for thetaNdx in range(number_of_angles):
+        theta = -math.pi/2 + thetaNdx * math.pi/number_of_angles
+        for rhoNdx in range(number_of_rhos):
+            rho = -rho_max + rhoNdx
+            line_integral = LineIntegral(density_arr, rho, theta, rho_max, center)
+            line_integrals_arr[number_of_rhos - rhoNdx - 1, thetaNdx] = line_integral  # Negative rho is at bottom, positive rho is at top
+    return line_integrals_arr
 
 
 if __name__ == '__main__':
@@ -75,7 +82,6 @@ if __name__ == '__main__':
     parser.add_argument("--imageSizeHW", help="The image size (Height, Width). Default: '(256, 256)'",
                         default='(256, 256)')
     parser.add_argument("--outputDirectory", help="The output directory. Default: './outputs_simulate_bga'", default='./outputs_simulate_bga')
-    parser.add_argument("--numberOfAngles", help="The number of angles. Default: 256", type=int, default=256)
     args = parser.parse_args()
 
     imageSizeHW = ast.literal_eval(args.imageSizeHW)
@@ -83,5 +89,5 @@ if __name__ == '__main__':
     main(
         imageSizeHW,
         args.outputDirectory,
-        args.numberOfAngles
+        #args.numberOfAngles
     )
